18c6ac7f881e51c9f2da8edc5833ee8c28dc84f4
flhannich <49130619+flhannich@users.noreply.github.com>
2021-02-09 10:18:55 +0100

    added scroll based classes

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,62 @@
-import Link from 'next/link'
+import { useRef, useEffect } from 'react';
+import Link from 'next/link';
 
 const Header = () => {
+  
+  const node = useRef();
+
+  
+  let tick = false;
+  let lastScroll = 0;
+  
+  let classScrollsTop = 'is-scrolling--up';
+  let classScrollsDown = 'is-scrolling';
+  
+  let offset = 100;
+
+  
+  useEffect(() => {
+
+    document.addEventListener('scroll', function(e) {
+      if (!tick) {
+        window.requestAnimationFrame(function() {
+          setState ()
+          tick = false;
+        });
+        tick = true;
+      }
+    });
+    
+  }, []);
+  
+
+  const setState = () => {
+    
+    let header = node.current;
+    
+    let scrollY = window.pageYOffset || document.documentElement.scrollTop;
+
+    (scrollY <= Math.max(lastScroll, 0))
+
+      ? header.classList.add(classScrollsTop)
+      : header.classList.remove(classScrollsTop);
+
+    (window.pageYOffset > offset)
+
+      ? header.classList.add(classScrollsDown)
+      : header.classList.remove(classScrollsDown, classScrollsTop);
+
+    lastScroll = scrollY;
+
+  }
+  
+  
   return (
     <>
 
-      <header className="">
+      <header 
+        ref={node}
+        className="">
 
         <div className="container">
 
