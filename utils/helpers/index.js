5e85446ae754b4abcbcd87c1321b7c9a71ffe0a9
flhannich <49130619+flhannich@users.noreply.github.com>
2021-02-09 14:38:25 +0100

    added lazyload for images and videos

--- utils/helpers/index.js
+++ utils/helpers/index.js
@@ -1,2 +1,3 @@
 export { default as lazyLoad } from "./lazyLoad";
 export { default as pageTransition } from "./pageTransition";
+export { default as scrollTrigger } from "./scrollTrigger";
--- utils/helpers/lazyload.js
+++ utils/helpers/lazyload.js
@@ -1,32 +1,62 @@
 
 const lazyLoad = () => {
-    
+      
     let callback = function (entries, observer) {
   
       entries.forEach (function (entry) {
-  
-      if (entry.isIntersecting) {
-  
-          let image = entry.target;
 
-          image.setAttribute('src', image.dataset.src);
-          image.removeAttribute('data-src');
+      if (entry.isIntersecting) {
+
+          let target = entry.target;
           
-          if(image.dataset.srcset) {
-          
-            image.setAttribute('srcset', image.dataset.srcset);
-            image.removeAttribute('data-srcset');
-          
+          if(target.tagName === 'VIDEO') {
+            let source = target.querySelectorAll('source');
+            source.forEach(function (item) {
+              item.setAttribute('src', item.dataset.src);
+              item.removeAttribute('data-src');
+            })
           }
           
-          image.classList.add('has-loaded');
-          observer.unobserve(image);
+  
+          if(target.dataset.src !== undefined) {
+              
+            let src = target.dataset.src;
+            let mime = src.split('.').pop();
+            
+            target.setAttribute('src', target.dataset.src);
+            target.removeAttribute('data-src');
+            
+            
+            if(mime === 'jpg' || mime === 'png') {
+              
+              let path = src.substr(0, src.lastIndexOf('/'));
+              let filename = src.split('/').pop().split('.')[0];
+
+              target.setAttribute('srcset', 
+                path + '/' + filename + '@0.3' + '.' + mime  + ' 780w,' +
+                path + '/' + filename + '@0.6' + '.' + mime  + ' 1280w,' +
+                path + '/' + filename + '@1' + '.' + mime  + ' 1680w'
+              );
+              
+            }
+   
+          }
+          target.classList.add('has-loaded');
+
+          // if(target.dataset.srcset) {
+            
+            // target.setAttribute('srcset', target.dataset.srcset);
+            // target.removeAttribute('data-srcset');
+          
+          // }
+          
+          observer.unobserve(target);
         }
       })
     }
   
-    let images = document.querySelectorAll('[data-src]');
-  
+    let elements = document.querySelectorAll('.lazy-load');
+    
     let options = {
       root: null,
       rootMargin: '0px 0px 50% 0px',
@@ -35,12 +65,13 @@ const lazyLoad = () => {
   
     let observer = new IntersectionObserver( callback, options);
   
-    images.forEach(function(image) {
+    elements.forEach(function(el) {
   
-    observer.observe(image);
+     observer.observe(el);
   
     });
-
-}
+    
+  }
+  
 
 export default lazyLoad;
