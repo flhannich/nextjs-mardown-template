936cd895d4b416ece19e168a02d3e08836b471f7
flhannich <49130619+flhannich@users.noreply.github.com>
2021-02-09 10:54:15 +0100

    added lazy loading

--- components/Image.js
+++ components/Image.js
@@ -1,24 +1,72 @@
-import React, { useEffect } from 'react';
-import * as utils from '../utils/helpers/lazyLoad'
+import React, { useRef, useEffect } from 'react';
 
-const Image = ({ alt, src }) => {
-
+const Image = ({ placeholder, src, srcset, alt }) => {
+  
+  const node = useRef();
+  
   useEffect(() => {
-    utils.lazyLoad();
-    window.addEventListener('scroll', utils.lazyLoad);
-    window.addEventListener('resize', utils.lazyLoad);
+
+    let callback = function (entries, observer) {
+  
+      entries.forEach (function (entry) {
+  
+      if (entry.isIntersecting) {
+  
+          let image = entry.target;
+  
+          image.setAttribute('src', image.dataset.src);
+          image.removeAttribute('data-src');
+          
+          if(image.dataset.srcset) {
+          
+            image.setAttribute('srcset', image.dataset.srcset);
+            image.removeAttribute('data-srcset');
+          
+          }
+          
+          node.current.classList.add('has-loaded');
+          observer.unobserve(image);
+        }
+      })
+    }
+  
+    let images = document.querySelectorAll('[data-src]');
+  
+    let options = {
+      root: null,
+      rootMargin: '0px 0px 50% 0px',
+      threshold: 0
+    }
+  
+    let observer = new IntersectionObserver( callback, options);
+  
+    images.forEach(function(image) {
+  
+    observer.observe(image);
+  
+    });
+  
+    // window.addEventListener('scroll', utils.lazyLoad);
+    // window.addEventListener('resize', utils.lazyLoad);
   });
+  
+
 
   return (
 
-    <span className="image-container">
+    <div 
+      className="image-holder"
+      ref={node}
+    >
 
       <img
+        src={placeholder}
         data-src={src}
+        data-srcset={srcset}
         alt={alt}
       />
 
-    </span>
+    </div>
 
   );
 };
